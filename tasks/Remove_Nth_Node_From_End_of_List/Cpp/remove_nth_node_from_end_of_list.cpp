e3fba671ccd7621834b7fed02a2ae207ca1fdfa1
caesiumCS <caesiummm@yandex.ru>
2023-04-21 10:04:22 +0300

    Remove Nth Node From End of List

---
+++ tasks/Remove_Nth_Node_From_End_of_List/Cpp/remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* left = head;
+        ListNode* right = head;
+
+        if (head->next == nullptr) return nullptr;
+
+
+        for (int i = 0; i < n; i++, right = right->next);
+
+        if (right == nullptr) {
+            left = left->next; 
+            return left;
+        }
+
+        while (right->next != nullptr) {
+            left = left->next;
+            right = right->next;
+        }
+
+        left->next = left->next->next;
+
+        return head;
+
+    }
+};
---
+++ tasks/Remove_Nth_Node_From_End_of_List/README.md
@@ -0,0 +1,12 @@
+#  Remove Nth Node From End of List
+[Link.](https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/)
+
+**Describtion**:
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+**Complexity of solution**:
+
+Memory: *O*(1)
+
+Time: *O*(*n*)
